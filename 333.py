2afca24ab456e42d65443aeba10f73aaa795c16b
Boburbek <83004593+Boburbek2363@users.noreply.github.com>
2021-04-22 14:21:29 -0930

    yangi fayila qoshosh
    birinchi kodim

---
+++ 333.py
@@ -0,0 +1,8 @@
+print ('Home work')
+
+for a in range(1, 1000):
+   for b in range(2, 1000):
+         print(f'{a} * {b} = {a * b}\t', end= '')
+   print('')
+else:
+   print('well') 
---
+++ clasnaya_rabota.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env/ python3
+
+
+def main():
+   imin1=int(input("Please enter first minimum:"))
+   imax1=int(input("Ple
